2ebbf8fd15ebfb0f1779eee9b2b648cce797b25e
Christof <bauer.christof@gmail.com>
2021-12-02 17:47:03 -0500

    Update team.js

--- pages/team.js
+++ pages/team.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Main from "../components/Main"
+import Social from '../components/Social'
 
 const Team = () => {
   return (
     <Main title='Team' titleSuffix={true}>
       <h1 className='text-white'>Team</h1>
+      <Social />
 
       <ul className="my-16 flex items-start justify-center text-center text-sm space-x-8 dark:text-white">
         <li className="w-1/3">
@@ -12,7 +14,7 @@ const Team = () => {
           <h2>L</h2>
           <p>Founder<br />Mastermind<br />Meme hunter</p>
         </li>
-        <li className="w-1/3">
+        <li className="w-1/3 hover:border-2 hover:shadow">
           <Image src="/team/jonny.png" width={200} height={200} alt="jonny" />
           <h2>J</h2>
           <p>Artist<br />Sincerely gifted<br />Best looking of the team</p>
