674fb1c15946bf45811125a17344a07c3e164284
Christof <bauer.christof@gmail.com>
2021-12-03 11:08:32 -0500

    Let's try another purge...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  purge: ["./pages/**/*.{js}"],
+  purge: [
+    "./pages/**/*.js",
+    "./pages/**/*.ts",
+    "./pages/**/*.jsx",
+    "./pages/**/*.tsx",
+    "./components/**/*.js",
+    "./components/**/*.ts",
+    "./components/**/*.jsx",
+    "./components/**/*.tsx"
+  ],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     fontFamily: {
